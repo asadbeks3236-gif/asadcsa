76edecdfc643f3d88d189c8f263e3f6c34962075
Asad <asadbeks3236@gmail.com>
2026-06-04 16:29:37 -0400

    Add method to shorten username by skipping hyphens
    Implement getShortenedName method to process username.

--- question 1/partB.java
+++ question 1/partB.java
@@ -1,1 +1,21 @@
+public String getShortenedName()
+{
+    String result = "";
+    int i = 0;
+
+    while (i < username.length()) {
+        if (username.charAt(i) == '-') {
+            // skip the character before '-' (already added) and the '-'
+            i++; // move past '-'
+        } else if (i + 1 < username.length() && username.charAt(i + 1) == '-') {
+            // current char is before a hyphen: skip adding it
+            i++; // move to '-'; loop will handle skipping it next iteration
+        } else {
+            result += username.charAt(i);
+            i++;
+        }
+    }
+
+    return result;
+}
 
